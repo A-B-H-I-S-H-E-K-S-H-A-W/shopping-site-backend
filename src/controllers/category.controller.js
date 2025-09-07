213f4537b0f1921874fd12397446001eb4b097b4
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-09-07 10:02:20 +0530

    Initialized necessaryn routes and controllers for product category

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,4 +1,4 @@
-import { Category } from "../models/category.models";
+import { Category } from "../models/category.models.js";
 
 export const createParentCategory = async (req, res) => {
   try {
@@ -207,6 +207,111 @@ export const updateCategory = async (req, res) => {
     });
   } catch (error) {
     console.log("Error updating category::::", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      statusCode: 500,
+    });
+  }
+};
+
+export const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid category ID",
+        statusCode: 400,
+      });
+    }
+
+    const child = await Category.findOne({ parentCategory: id });
+    if (child) {
+      return res.status(400).json({
+        success: false,
+        message: "Cannot delete category with subcategories",
+        statusCode: 400,
+      });
+    }
+
+    const category = await Category.findById(id);
+
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: "Category not found",
+        statusCode: 404,
+      });
+    }
+    await Category.deleteOne({ _id: id });
+    return res.status(200).json({
+      success: true,
+      message: "Category deleted successfully",
+      statusCode: 200,
+    });
+  } catch (error) {
+    console.log("Error deleting category::::", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      statusCode: 500,
+    });
+  }
+};
+
+export const getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.find().populate(
+      "parentCategory",
+      "name _id"
+    );
+    return res.status(200).json({
+      success: true,
+      message: "Categories fetched successfully",
+      statusCode: 200,
+      data: categories,
+    });
+  } catch (error) {
+    console.log("Error fetching categories::::", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      statusCode: 500,
+    });
+  }
+};
+
+export const getCategoryById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid category ID",
+        statusCode: 400,
+      });
+    }
+    const category = await Category.findById(id).populate(
+      "parentCategory",
+      "name _id"
+    );
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: "Category not found",
+        statusCode: 404,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Category fetched successfully",
+      statusCode: 200,
+      data: category,
+    });
+  } catch (error) {
+    console.log("Error fetching category::::", error);
     res.status(500).json({
       success: false,
       message: "Internal Server Error",
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -2,11 +2,17 @@ import { Router } from "express";
 import {
   createChildCategory,
   createParentCategory,
+  deleteCategory,
+  getAllCategories,
+  getCategoryById,
   updateCategory,
-} from "../controllers/category.controller";
+} from "../controllers/category.controller.js";
 
 export const categoryRouter = Router();
 
 categoryRouter.post("/create-parent", createParentCategory);
 categoryRouter.post("/create-child", createChildCategory);
 categoryRouter.put("/update/:id", updateCategory);
+categoryRouter.delete("/delete/:id", deleteCategory);
+categoryRouter.get("/all-categories", getAllCategories);
+categoryRouter.get("/item/:id", getCategoryById);
