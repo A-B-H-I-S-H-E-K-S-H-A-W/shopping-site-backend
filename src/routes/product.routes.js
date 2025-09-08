c66b5d02c7e45fcad7c7c71bc1b98bf6e1342628
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-09-08 23:17:07 +0530

    Fixed issues and connected route to controller

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { createProduct } from "../controllers/product.controller.js";
-import { deleteCategory } from "../controllers/category.controller.js";
+import {
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} from "../controllers/product.controller.js";
 
 export const productRouter = Router();
 
 productRouter.post("/create", createProduct);
-productRouter.delete("/delete/:id", deleteCategory);
+productRouter.delete("/delete/:id", deleteProduct);
+productRouter.put("/update/:id", updateProduct);
